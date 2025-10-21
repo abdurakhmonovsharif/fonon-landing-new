fc19e76df2e5a318fb3898acf03a871061ff863d
abdurakhmonovsharif <fullstackdeveloper4413@gmail.com>
2025-10-21 14:21:36 +0500

    update api

--- src/lib/redux/services/api.js
+++ src/lib/redux/services/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:9090/api/',
+        baseUrl: 'http://185.217.131.110:9090/api/',
         prepareHeaders: (headers, { getState }) => {
             const token = localStorage.getItem('token');
             if (token) {
