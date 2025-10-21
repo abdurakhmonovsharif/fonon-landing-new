d51df22fb0dc942dfe5ebae46c45ddd4fb941426
abdurakhmonovsharif <fullstackdeveloper4413@gmail.com>
2025-10-21 14:28:28 +0500

    a

--- src/lib/redux/services/api.js
+++ src/lib/redux/services/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
     reducerPath: 'baseApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://185.217.131.110:9090/api/',
+        baseUrl: '/api/',
         prepareHeaders: (headers, { getState }) => {
             const token = localStorage.getItem('token');
             if (token) {
